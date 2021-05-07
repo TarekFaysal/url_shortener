51e926484ccc5e715e27d36a53d10b3b81511d0b
Tarek Faysal <faysal5467@gmail.com>
2021-05-07 11:56:37 +0600

    generate short url and redirect to long url apis are created

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
-//short url working
-
+//redirect to long url
 const express = require('express');
 const router = express.Router();
+
+const Url = require('../models/Url');
+
+//@route        GET /:CODE
+//@desc         Redirect to long/original URL
+router.get('/:code', async(req,res)=>{
+    try {
+        const url = await Url.findOne({urlCode: req.params.code});
+        
+        if(url){
+            return res.redirect(url.longUrl);
+        }else{
+            return res.status(404).json('No url found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Server error');
+    }
+});
 
 module.exports = router;
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,56 @@
-// generate short url from long url
-//short url working
+//generate short url
 
 const express = require('express');
 const router = express.Router();
+const validUrl = require('valid-url');
+const shortid = require('shortid');
+const config = require('config');
+
+const Url = require('../models/Url');
+
+//@route    POST /api/url/shorten
+//@desc     Create short URL
+router.post('/shorten', async (req,res)=>{
+    const { longUrl } = req.body;
+    const baseUrl = config.get('baseurl');
+
+    //check base url
+    if(!validUrl.isUri(baseUrl)){
+        return res.status(400).json('Invalid base Url');
+    }
+
+    //create url code
+    const urlCode = shortid.generate();
+
+
+    //check long url
+    if(validUrl.isUri(longUrl)){
+        try {
+            let url = await Url.findOne({longUrl});
+
+            if(url){
+                res.json(url);
+            }else{
+                const shortUrl = baseUrl + '/' + urlCode;
+
+                url = new Url({
+                    longUrl,
+                    shortUrl,
+                    urlCode,
+                    date: new Date()
+                });
+
+                await url.save();
+
+                res.json(url);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json("Server error");
+        }
+    } else{
+        res.status(400).json('Invalid long url');
+    }
+});
 
 module.exports = router;
